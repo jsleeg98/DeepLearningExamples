a4ef28210cd17fa884fa7939798920c428be91a0
DHLEE_V100 <jsleeg98@cbnu.ac.kr>
2023-06-13 14:36:04 +0900

    Fix bug
    forward pass through scale0

--- PyTorch/Classification/ConvNets/image_classification/models/resnet.py
+++ PyTorch/Classification/ConvNets/image_classification/models/resnet.py
@@ -591,6 +591,7 @@ class ResNet_DBC_share(nn.Module):
         if self.bn1 is not None:
             x = self.bn1(x)
         x = self.relu(x)
+        x = self.scale0(x)
         x = self.maxpool(x)
         return x
 
